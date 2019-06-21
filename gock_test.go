1faf6d3884bb1af99d373607a9654aa4e7f9e9f6
Toni Cárdenas <toni@tcardenas.me>
2019-06-21 09:43:10 +0200

    Complete test coverage.

--- gock_test.go
+++ gock_test.go
@@ -63,6 +63,176 @@ func TestGoRunsBeforeWait(t *testing.T) {
 	<-done
 }
 
+func TestGoAfterWait(t *testing.T) {
+	g, wait := gock.Bundle()
+	wait()
+	func() {
+		defer func() {
+			recover()
+		}()
+		g(func() error { return nil })
+		t.Error("expected panic")
+	}()
+}
+
+func TestIdempotentWait(t *testing.T) {
+	expected := errors.New("expect me")
+	g, wait := gock.Bundle()
+	timesRun := 0
+	g(func() error {
+		timesRun++
+		return expected
+	})
+	for i := 0; i < 2; i++ {
+		err := wait()
+		if expected != err {
+			t.Errorf("expected to get the same error twice, got %s on #%d", err, i)
+		}
+	}
+	if timesRun != 1 {
+		t.Errorf("expected function to only be run once, got run %d", timesRun)
+	}
+}
+
+func TestBundleNothing(t *testing.T) {
+	_, wait := gock.Bundle()
+	err := wait()
+	if err != nil {
+		t.Errorf("got unexpected error: %s", err)
+	}
+}
+
+func TestWaitForNothing(t *testing.T) {
+	err := gock.Wait()
+	if err != nil {
+		t.Errorf("got unexpected error: %s", err)
+	}
+}
+
+func TestConcurrentErrorsString(t *testing.T) {
+	err := gock.AddConcurrentError(errors.New("foo"), errors.New("bar"))
+	if expected, got := "concurrent errors: foo; bar", err.Error(); expected != got {
+		t.Errorf("expected: %q got: %q", expected, got)
+	}
+}
+
+func TestConcurrentErrorsFlatten(t *testing.T) {
+	errs := []error{errors.New("foo"), errors.New("bar"), errors.New("baz")}
+	cerrs := gock.AddConcurrentError(
+		errs[0],
+		gock.AddConcurrentError(
+			gock.AddConcurrentError(
+				nil,
+				errs[1],
+			),
+			errs[2],
+		),
+	).(gock.ConcurrentErrors)
+	if expected, got := 3, len(cerrs.Errors); expected != got {
+		t.Errorf("expected %d flattened ConcurrentErrors, got %d", expected, got)
+	}
+	for _, err := range cerrs.Errors {
+		if _, ok := err.(gock.ConcurrentErrors); ok {
+			t.Errorf("this ConcurrentErrors wasn't flattened: %s", err)
+		}
+	}
+}
+
+type chain struct {
+	err1, err2 error
+}
+
+func (c chain) Error() string { return fmt.Sprintf("%v: %v", c.err1, c.err2) }
+func (c chain) Unwrap() error { return c.err2 }
+
+func TestAnyIs(t *testing.T) {
+	expected := errors.New("expect me")
+
+	err := gock.Wait(func() error {
+		return errors.New("I'm not")
+	}, func() error {
+		return chain{errors.New("wrapping: "), expected}
+	})
+
+	ok := gock.AnyIs(err, expected)
+	if !ok {
+		t.Error("should find the expected error")
+	}
+}
+
+func TestAnyIsNot(t *testing.T) {
+	notFound := errors.New("won't find me")
+
+	err := gock.Wait(func() error {
+		return errors.New("I'm not")
+	}, func() error {
+		return chain{errors.New("wrapping: "), errors.New("me neither")}
+	})
+
+	ok := gock.AnyIs(err, notFound)
+	if ok {
+		t.Error("shouldn't find the error")
+	}
+}
+
+func TestAnyIsSingle(t *testing.T) {
+	expected := errors.New("expect me")
+
+	ok := gock.AnyIs(expected, expected)
+	if !ok {
+		t.Error("should find the expected error")
+	}
+}
+
+type myError string
+
+func (err myError) Error() string { return "am an error: " + string(err) }
+
+func TestAnyAs(t *testing.T) {
+	expected := myError("expect me")
+	err := gock.Wait(func() error {
+		return errors.New("I'm not")
+	}, func() error {
+		return chain{errors.New("wrapping: "), expected}
+	})
+
+	var got myError
+	ok := gock.AnyAs(err, &got)
+	if !ok {
+		t.Error("should find the myError")
+	}
+	if expected != got {
+		t.Errorf("expected: %v, got: %v", expected, got)
+	}
+}
+
+func TestAnyAsNot(t *testing.T) {
+	err := gock.Wait(func() error {
+		return errors.New("I'm not")
+	}, func() error {
+		return chain{errors.New("wrapping: "), errors.New("me neither")}
+	})
+
+	var got myError
+	ok := gock.AnyAs(err, &got)
+	if ok {
+		t.Error("shouldn't find a myError")
+	}
+}
+
+func TestAnyAsSingle(t *testing.T) {
+	expected := myError("expect me")
+
+	var got myError
+	ok := gock.AnyAs(expected, &got)
+	if !ok {
+		t.Error("should find the myError")
+	}
+	if expected != got {
+		t.Errorf("expected: %v, got: %v", expected, got)
+	}
+}
+
 func TestConcurrentErrorsUnwrapNoCommonAncestor(t *testing.T) {
 	ancestor := errors.New("ancestor")
 	err := gock.AddConcurrentError(
